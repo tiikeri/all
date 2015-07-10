73fb43ace62ae4fc609cd374ecdcd393fffb5ea4
viktorahlstroem <sno.kattunge@gmail.com>
2015-07-10 00:34:41 -0400

    # 3and5.sh

--- coding-problems-solved/eulers/3and5.sh
+++ coding-problems-solved/eulers/3and5.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # project euler #1
-numbers=()
-mult=()
-for i in {1..999} ; do
+numbers=() #create the array for all the numbers
+mult=() #create an array for the multiples
+for i in {1..999} ; do #set up the numbers array
 	numbers[((i-1))]=$i
 done
 for i in {0..998} ; do
-	alreadyIn=0
-	if [[ $((${numbers[$i]}%3)) == 0 ]] ; then
-        mult+=" ${numbers[$i]} "
-	alreadyIn=1;
+	alreadyIn=0 #set a variable to ensure the number was not already put into the array
+	if [[ $((${numbers[$i]}%3)) == 0 ]] ; then #check if it is a multiple of three
+        mult+=" ${numbers[$i]} " #add it to the array
+	alreadyIn=1; #signify that the number has been added
 	fi
-	if [[ $((${numbers[$i]}%5)) == 0 ]] && [[ $alreadyIn -eq 0 ]] ; then
-		mult+=" ${numbers[$i]} "
+	if [[ $((${numbers[$i]}%5)) == 0 ]] && [[ $alreadyIn -eq 0 ]] ; then #check if the number is a multiple of five and hasn't already been added
+		mult+=" ${numbers[$i]} " #add it to the array
 	fi
-
 done
-echo ${mult[*]}
-for i in ${mult[@]}; do
+echo ${mult[*]} #display the multiples
+for i in ${mult[@]}; do #add them together
   let tot+=$i
 done
-echo Total of sums: $tot
+echo Sum: $tot #display the sum
+exit 1
