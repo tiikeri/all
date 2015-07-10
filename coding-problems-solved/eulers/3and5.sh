e4a6171af7238ca5854d534ed323eaed2e069a1a
viktorahlstroem <sno.kattunge@gmail.com>
2015-07-10 00:40:32 -0400

    ~ 3and5.sh; # sumsd.sh

--- coding-problems-solved/eulers/3and5.sh
+++ coding-problems-solved/eulers/3and5.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # project euler #1
-numbers=() #create the array for all the numbers
-mult=() #create an array for the multiples
-for i in {1..999} ; do #set up the numbers array
+numbers=()                    #create the array for all the numbers
+mult=()                       #create an array for the multiples
+for i in {1..999} ; do         #set up the numbers array
 	numbers[((i-1))]=$i
 done
 for i in {0..998} ; do
-	alreadyIn=0 #set a variable to ensure the number was not already put into the array
-	if [[ $((${numbers[$i]}%3)) == 0 ]] ; then #check if it is a multiple of three
-        mult+=" ${numbers[$i]} " #add it to the array
-	alreadyIn=1; #signify that the number has been added
+	alreadyIn=0                                   #set a variable to ensure the number was not already put into the array
+	if [[ $((${numbers[$i]}%3)) == 0 ]] ; then    #check if it is a multiple of three
+        mult+=" ${numbers[$i]} "                #add it to the array
+	alreadyIn=1;                                  #signify that the number has been added
 	fi
 	if [[ $((${numbers[$i]}%5)) == 0 ]] && [[ $alreadyIn -eq 0 ]] ; then #check if the number is a multiple of five and hasn't already been added
-		mult+=" ${numbers[$i]} " #add it to the array
+		mult+=" ${numbers[$i]} "                                           #add it to the array
 	fi
 done
-echo ${mult[*]} #display the multiples
-for i in ${mult[@]}; do #add them together
+echo ${mult[*]}           #display the multiples
+for i in ${mult[@]}; do   #add them together
   let tot+=$i
 done
-echo Sum: $tot #display the sum
+echo Sum: $tot             #display the sum
 exit 1
--- coding-problems-solved/eulers/sumsd.sh
+++ coding-problems-solved/eulers/sumsd.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # project euler #6
-numbers=()
-squared=()
-sumOf=()
+numbers=()                                        #make an array for the numbers
+squared=()                                        #make an array for the numbers squared
+sumOf=()                                          #make an array for the sum of all the numbers
 
-for i in {1..100} ; do
+for i in {1..100} ; do                            #set up the array for the numbers
 	numbers[((i-1))]=$i
 done
-for i in {0..99} ; do
+for i in {0..99} ; do                             #set up the array for the numbers squared
 	   x=${numbers[$i]}
 	   x=$(($x*$x))
 	   squared+=" $x "
 		   
 done
-echo ${squared[*]}
-for i in ${squared[@]}; do
-  let sumOf+=$i
+echo ${squared[*]}                                #display all of the numbers squared
+for i in ${squared[@]}; do                        #add all of the numbers squared together
+  let sumOf+=$i                                   
 done
-for i in ${numbers[@]}; do
+for i in ${numbers[@]}; do                        #add all of the regular numbers together
   let sumOfN+=$i
 done
 
-echo Total of sums: $sumOf
-sumSquared=$(($sumOfN*$sumOfN))
-echo Sum squared: $sumSquared
-echo Difference: $(($sumSquared-$sumOf))
+echo Sum of 1^2..100^2: $sumOf                        #display the first sum
+sumSquared=$(($sumOfN*$sumOfN))                       #calculate sum squared
+echo Sum of 1..100 squared: $sumSquared               #display sum squred
+echo Difference: $(($sumSquared-$sumOf))              #display the difference
